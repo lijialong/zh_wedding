8911167f94f17d9470573c02a3231bc0936f7f30
Leif <919820268@qq.com>
2019-01-22 12:39:30 +0800

    添加注释

--- get_zhsj.py
+++ get_zhsj.py
@@ -3,14 +3,14 @@ import requests
 
 header = {
 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0",
-"Accept-Encoding": "gzip",
+"Accept-Encoding": "gzip", #指定gzip编码，br编码requests无法识别
 "x-requested-with": "fetch"
 }
 url = "https://www.zhihu.com/api/v4/questions/275359100/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=100&offset=0&platform=desktop&sort_by=default"
 
 session = requests.session()
-session.headers = header
+session.headers = header #添加请求头
 repl = session.get(url)
-content = json.loads(repl.content)
+content = json.loads(repl.content) #以json格式接收传回文件
 print(content['paging']['next'])
 
